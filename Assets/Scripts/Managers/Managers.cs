fdf1a6b10c0eaa46c1784659c87011f9f14203df
LDHEBONY <cjg0513396@gmail.com>
2025-12-05 04:22:23 +0900

    버튼 이벤트 할당 준비
    버튼 온클릭 이벤트 할당 준비만함

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a12a72ec4165b09f4eb7989a3a130390995b2572dbc5b3cc22257216847eccb
-size 2746
+oid sha256:bf9a3724e8aae9183230116b99b63d2be3250c4578ad019f027524a4f14c53bb
+size 2783
