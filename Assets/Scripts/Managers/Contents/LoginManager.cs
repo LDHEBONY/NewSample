bbd0b1e2c500aa3a9bb46492641dd8b9c5605628
LDHEBONY <cjg0513396@gmail.com>
2025-12-18 03:15:17 +0900

    UGS 성공여부 추가
    이제 순환구조 만들어야됨

--- Assets/Scripts/Managers/Contents/LoginManager.cs
+++ Assets/Scripts/Managers/Contents/LoginManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f45f247d715b266be184fffe850daac1839850bd0df6b152ddd55f4c574bc74
-size 2533
+oid sha256:bf45e7ed2209db701c42063651c407fe92efd6ac565728adde31667fce56e59b
+size 2996
