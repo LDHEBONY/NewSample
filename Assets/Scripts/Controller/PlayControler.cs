4e4c02ff0d14c42ace9b0e253d279b5a8a38291d
LDHEBONY <cjg0513396@gmail.com>
2026-03-11 04:41:45 +0900

    인벤토리 여닫이 기능 추가
    I키로 여닫기 기능 추가

--- Assets/Scripts/Controller/PlayControler.cs
+++ Assets/Scripts/Controller/PlayControler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:066431a1868b756a9af99884c2b9389d79b44415bbcf11c3221eda41abe3a451
-size 14574
+oid sha256:23ae9038e29039cde916f3db1885b90382316c6726b6f8b6ba15d7dbebc41ec6
+size 14718
--- Assets/Scripts/Managers/Contents/Spawnsite.cs
+++ Assets/Scripts/Managers/Contents/Spawnsite.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9d7f08cb0487e5a8453f3b4eefeec1464bd2cffff9348fdcbc87b17eef320e3
-size 3589
+oid sha256:a9436e3681f10140fbd6586227bd1b8cdfa25865ca04431aa45d5798886b849e
+size 3851
