5797ce9a602a345ca276c5ca770a1e30ab330d1a
LDHEBONY <cjg0513396@gmail.com>
2026-05-26 04:14:51 +0900

    몬스터 투사체 분기 준비
    공격분기는 했고, 투사체를 다르게 하기만 하면됨

--- Assets/Scripts/Controller/MonsterController.cs
+++ Assets/Scripts/Controller/MonsterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e757928cc57d0020f946dc1e0319388a6560c9e93ce6f59dc21ea02b76d5066f
-size 15730
+oid sha256:a9f250efdf5df3210fd00ac1ea552068f8294623b87bdb9a78c86973e2fe9263
+size 15654
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44a2cdcc4063d5ddaba7d727767d6d7de6308c182de66344433bc16ccf77541f
-size 1356
+oid sha256:0411a3b92871883f3160f6f70086cd52da260bc94bf6d0051caba59d4e68bdec
+size 1578
