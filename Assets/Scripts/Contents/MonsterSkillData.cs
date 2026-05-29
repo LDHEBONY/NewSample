f1f651fe36b036898e001c0c091aba48991e708c
LDHEBONY <cjg0513396@gmail.com>
2026-05-30 02:52:18 +0900

    몬스터 스킬데이터 클래스 생성
    스킬데이터 만들어서 한 컨트롤러에서 확장시켜 사용할예정

---
+++ Assets/Scripts/Contents/MonsterSkillData.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:518b00ed994d15ca9980d94b54ea27c12fa85cb82411d0142eafa4425258864b
+size 497
--- Assets/Scripts/Controller/MonsterController.cs
+++ Assets/Scripts/Controller/MonsterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9f250efdf5df3210fd00ac1ea552068f8294623b87bdb9a78c86973e2fe9263
-size 15654
+oid sha256:4402b77fb87af2f2fc19a9be7d99d4fbc83e82cc6d7cdcff9e6153e439691151
+size 15641
