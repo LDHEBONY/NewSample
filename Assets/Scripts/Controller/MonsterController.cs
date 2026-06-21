70a07cbd380217f79331e1df1c0346a0184f85ac
LDHEBONY <cjg0513396@gmail.com>
2026-06-22 04:36:32 +0900

    투사체 던지기 기능 구현
    투사체 사출 및 오브젝트 풀링 구현
    데미지 넣는 기능은 추가예정

--- Assets/Scripts/Controller/MonsterController.cs
+++ Assets/Scripts/Controller/MonsterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4402b77fb87af2f2fc19a9be7d99d4fbc83e82cc6d7cdcff9e6153e439691151
-size 15641
+oid sha256:09652556ec1613aea40a0a3ace55e3f89c3f6faaf1514eb960faa8c32154fec1
+size 15822
